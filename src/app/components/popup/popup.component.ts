187963c7eaf7160de07f83ceb93cb3d5dcc8a49e
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-03 13:24:49 -0300

    ✨ feat: popup page

---
+++ src/app/components/popup/popup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.scss']
+})
+export class PopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
