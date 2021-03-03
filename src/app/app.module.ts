b253a6b4df4447f160f6a41b6a69ade875d1dec3
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-03 18:46:04 -0300

    ✨ feat: popup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
@@ -16,6 +17,7 @@ import { PopupComponent } from './components/popup/popup.component';
     MatInputModule,
     MatIconModule,
     ReactiveFormsModule,
+    MatButtonModule,
   ],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.scss']
+  styleUrls: ['./popup.component.scss'],
 })
 export class PopupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  wordControl: FormControl;
+  constructor() {
+    this.wordControl = new FormControl('', [Validators.required]);
   }
 
+  ngOnInit(): void {}
 }
