143d5786384535824ae539c267c431452a0f4b6d
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-03 14:32:24 -0300

    ✨ feat: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OptionsComponent } from './components/options/options.component';
 import { PopupComponent } from './components/popup/popup.component';
-
 @NgModule({
   declarations: [AppComponent, PopupComponent, OptionsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
