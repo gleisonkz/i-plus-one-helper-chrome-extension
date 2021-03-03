65f5b395c0631e41666f037fb7c24a0b675c665a
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-03 14:10:44 -0300

    📦 refactor: change to hash location strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OptionsComponent } from './components/options/options.component';
+import { PopupComponent } from './components/popup/popup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'popup', component: PopupComponent },
+  { path: 'options', component: OptionsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PopupComponent } from './components/popup/popup.component';
 import { OptionsComponent } from './components/options/options.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PopupComponent,
-    OptionsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PopupComponent, OptionsComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
