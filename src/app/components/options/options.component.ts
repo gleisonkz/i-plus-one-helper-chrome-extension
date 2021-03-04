ed783a86b64ad5ecb459d73c3be29f7208da53a4
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-04 16:58:58 -0300

    ✨ feat: add options page

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -1,14 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './options.component.html',
-  styleUrls: ['./options.component.scss']
+  styleUrls: ['./options.component.scss'],
 })
 export class OptionsComponent implements OnInit {
+  panelOpenState = true;
+  urlControl: FormControl;
+  urls = [
+    `https://www.wordreference.com/enpt/[word]`,
+    `https://context.reverso.net/traducao/ingles-portugues/[word]`,
+    `https://www.oxfordlearnersdictionaries.com/us/definition/english/[word]?q=[word]`,
+    `https://dictionary.cambridge.org/pt/dicionario/ingles/[word]`,
+    `https://translate.google.com.br/?hl=pt&sl=en&tl=pt&text=[word]&op=translate`,
+    `https://youglish.com/pronounce/[word]/english?`,
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.urlControl = new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern(/^https?:\/\//),
+    ]);
   }
 
+  addUrl() {
+    this.urlControl.valid && this.urls.push(this.urlControl.value);
+    this.urlControl.setValue('');
+  }
+
+  word = 'ruthless';
+
+  visible = true;
+  selectable = true;
+  removable = true;
+
+  ngOnInit(): void {}
+
+  remove(fruit: string): void {
+    // const index = this.fruits.indexOf(fruit);
+    // if (index >= 0) {
+    //   this.fruits.splice(index, 1);
+    // }
+  }
 }
