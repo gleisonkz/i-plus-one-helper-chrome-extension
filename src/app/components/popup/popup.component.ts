3f7cc66686be1efe45ae67c4865b40b7b081b372
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-04 11:15:41 -0300

    ✨ feat: open multiple urls

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -7,9 +7,34 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class PopupComponent implements OnInit {
   wordControl: FormControl;
+  @ViewChild('input', { static: true }) $input: ElementRef<HTMLInputElement>;
+
   constructor() {
-    this.wordControl = new FormControl('', [Validators.required]);
+    this.wordControl = new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.$input.nativeElement.focus();
+  }
+
+  search() {
+    if (this.wordControl.invalid) return;
+    const word = this.wordControl.value;
+
+    const targetUrls = [
+      `https://www.wordreference.com/enpt/${word}`,
+      `https://context.reverso.net/traducao/ingles-portugues/${word}`,
+      `https://www.oxfordlearnersdictionaries.com/us/definition/english/${word}?q=${word}`,
+      `https://dictionary.cambridge.org/pt/dicionario/ingles/${word}`,
+      `https://translate.google.com.br/?hl=pt&sl=en&tl=pt&text=${word}&op=translate`,
+      `https://youglish.com/pronounce/${word}/english?`,
+    ];
+
+    targetUrls.forEach((url) => {
+      chrome.tabs.create({ url: url });
+    });
+  }
 }
