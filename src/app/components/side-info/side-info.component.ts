27537287deb77c8d38a83a26958dfd55b95150c3
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-04-20 14:58:43 -0300

    📦 refactor: extract side-info component

---
+++ src/app/components/side-info/side-info.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'side-info',
+  templateUrl: './side-info.component.html',
+  styleUrls: ['./side-info.component.scss'],
+})
+export class SideInfoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
